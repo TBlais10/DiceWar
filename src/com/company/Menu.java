69c938b20a5339bda04086731d622090093546cb
TBlais10 <blaislighting@gmail.com>
2021-09-09 21:02:45 -0400

    The menu was refactored to slim the code in the user input for the new dice war parameters. The remainder of the confirmation logic was stored in new method.

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -20,14 +20,14 @@ public class Menu {
     public static void mainMenu() {
         scoreboard();
         System.out.println("Would you like to start a new Dice War?!!! Y/N?");
-        String input = CLI.getString(1,4);
-        if (input.substring(0, 1).equalsIgnoreCase("Y")){
+        String input = CLI.getString(1, 4);
+        if (input.substring(0, 1).equalsIgnoreCase("Y")) {
             CLI.flavorText("Great! How many players will there be? Min of 2 players.");
-            int players = CLI.getInt(2,10);
+            int players = CLI.getInt(2, 10);
             System.out.println();
 
             CLI.flavorText("Ok! How many rounds do you want to play? Min of 1 round, max of 10.");
-            int rounds = CLI.getInt(1,10);
+            int rounds = CLI.getInt(1, 10);
             System.out.println();
 
             CLI.flavorText("Awesome! What type of dice do you want to use? Enter a number representing how many sides the dice has. Min of 2, max of 20.");
@@ -35,34 +35,32 @@ public class Menu {
             System.out.println();
 
             CLI.flavorText("Almost done! How many dice will each player roll per round? Min of 1, max of 10.");
-            int diceAmount = CLI.getInt(1,10);
+            int diceAmount = CLI.getInt(1, 10);
             System.out.println();
 
-            System.out.println("Ok, lets review!\nNumber of players: " + players + "\nNumber of Rounds: " + rounds + "\nType of dice your rolling: D" + typeOfDice + "\nThe amount of D" + typeOfDice + " : " + diceAmount); //TODO: Refactor this code into a new method
-            System.out.println("Is this all correct?\n Y to start the game or N to return to restart the form.");
-            //TODO: Refactor this input code into a new method
-            CLI.scanner.nextLine();
-            String answer = CLI.getString(1, 3);
-            if (answer.substring(0, 1).equalsIgnoreCase("Y")){
-                CLI.flavorText("Excellent! Lets get rolling!!!");
-                scoreboard.clear();
-                new Game(players, rounds, typeOfDice, diceAmount);
-            }
-            else if (answer.substring(0, 1).equalsIgnoreCase("N")){
-                CLI.flavorText("Ok! Restarting the form...");
-                //add method once code has been refactored
-            }
+            newDiceWarConfirmation(players, rounds, typeOfDice, diceAmount);
 
-        }
-
-        else if(input.substring(0,1).equalsIgnoreCase("N")){
+        } else if (input.substring(0, 1).equalsIgnoreCase("N")) {
             CLI.exit();
+        } else if (input.equalsIgnoreCase("test")) {
+            new Game(2, 3, 6, 4);
         }
 
-        else if (input.equalsIgnoreCase("test")){
-            new Game(2,3, 6,4);
-        }
+    }
 
+    private static void newDiceWarConfirmation(int players, int rounds, int typeOfDice, int diceAmount) {
+        System.out.println("Ok, lets review!\nNumber of players: " + players + "\nNumber of Rounds: " + rounds + "\nType of dice your rolling: D" + typeOfDice + "\nThe amount of D" + typeOfDice + " : " + diceAmount);
+        System.out.println("Is this all correct?\n Y to start the game or N to return to restart the form.");
+        CLI.scanner.nextLine();
+        String answer = CLI.getString(1, 3);
+        if (answer.substring(0, 1).equalsIgnoreCase("Y")) {
+            CLI.flavorText("Excellent! Lets get rolling!!!");
+            scoreboard.clear();
+            new Game(players, rounds, typeOfDice, diceAmount);
+        } else if (answer.substring(0, 1).equalsIgnoreCase("N")) {
+            CLI.flavorText("Ok! Restarting the form...");
+            //add method once code has been refactored
+        }
     }
 
     public static void getDiceQuote() {
